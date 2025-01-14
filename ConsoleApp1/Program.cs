1abd60e0a031812680dd012fe88a3c43e194d2bf
kola524 <bkola684@gmail.com>
2025-01-14 10:10:59 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lessons
+{
+class Program
+{
+    static void Main(string[] args)
+        {
+            //Запрос кода для программы
+            Console.WriteLine("Ведите код");
+            String a = Console.ReadLine();
+            //Вывод кода который ввел пользователь
+            Console.WriteLine(a);
+            
+        }
+}
+}
